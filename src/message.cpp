b7d2d2b7a55c20360a142c62689a6ca41457fac0
igordelatorre <delatorreigor0@gmail.com>
2023-08-19 18:47:29 -0300

    add message body

--- src/message.cpp
+++ src/message.cpp
@@ -12,8 +12,9 @@ Message Message::decode(string data)
     string sender_hostname = data_tokens[3];
     int port = stoi(data_tokens[4]);
     long sender_id = stol(data_tokens[5]);
+    optional<string> body = data_tokens[6] == "" ? nullopt : optional<string>{data_tokens[6]};
 
-    Message decodedMessage(message_type, ip, mac_address, sender_hostname, port, sender_id);
+    Message decodedMessage(message_type, ip, mac_address, sender_hostname, port, sender_id, body);
     return decodedMessage;
 }
 
@@ -24,9 +25,10 @@ string Message::encode()
     string str_message_type = to_string(static_cast<int>(_message_type));
     string str_port = to_string(_port);
     string str_sender_id = to_string(_sender_id);
+    string str_body = _body.has_value() ? _body.value() : "";
 
     return str_message_type + ";" + _ip + ";" + _mac_address + ";" + _sender_hostname + ";" + str_port + ";" +
-           str_sender_id;
+           str_sender_id + ";" + str_body;
 }
 
 // Constructors:
@@ -39,6 +41,19 @@ Message::Message(MessageType message_type, string ip, string mac_address, string
     _sender_hostname = sender_hostname;
     _port = port;
     _sender_id = sender_id;
+    _body = {};
+};
+
+Message::Message(MessageType message_type, string ip, string mac_address, string sender_hostname, int port,
+                 long sender_id, optional<string> body)
+{
+    Message::_message_type = message_type;
+    _ip = ip;
+    _mac_address = mac_address;
+    _sender_hostname = sender_hostname;
+    _port = port;
+    _sender_id = sender_id;
+    _body = body;
 };
 
 // Accessors:

a8523b2f3dc7f8579449041c706e7fa1d79342ac
Pietro Carrara <pbcarrara@inf.ufrgs.br>
2023-09-11 19:54:34 -0300

    Update ports

--- src/consts.h
+++ src/consts.h
@@ -2,8 +2,8 @@
 #define CONSTS_H
 
 #include "socket.h"
-                           
-constexpr Port SEND_PORT = 11234;
-constexpr Port RECEIVE_PORT = 11234;
+
+constexpr Port SEND_PORT = 14321;
+constexpr Port RECEIVE_PORT = 14321;
 
 #endif
